c74c99b041a637ac1d469b4679530e49496c0ab4
unknown <ilyas.savli47@gmail.com>
2020-08-30 21:47:41 +0300

    First Commit.

---
+++ Bussiness/Concrete/CategoryManager.cs
@@ -0,0 +1,23 @@
+﻿using Bussiness.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.Concrete
+{
+    class CategoryManager : ICategoryService
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryManager(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+
+        }
+        public List<Category> GetAll()
+        {
+            return _categoryService.GetAll();
+        }
+    }
+}
---
+++ Bussiness/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.Concrete
+{
+    class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public List<Product> GetByCategory(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryId == categoryId);
+        }
+    }
+}
